6f0f7ad4baf333bb364d2216b0f9cdef36fb8514
Stas Martynov <maks.mart71@bk.ru>
2023-02-22 09:32:34 +0400

    закомител домашние задание GB_C#_03

---
+++ C#_DZ_03_01/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да*/
+System.Console.WriteLine("Введите пятизначное число: ");
+string? Namber = Console.ReadLine();
+if (Namber?.Length == 5 && Namber[0] == Namber[4] && Namber[1] == Namber[3])
+{
+    System.Console.WriteLine($"Число: {Namber}-зеркальнное");
+}
+else if (Namber?.Length == 5 && Namber[0] != Namber[4] && Namber[1] != Namber[3])
+{
+    System.Console.WriteLine($"Число: {Namber}-не зеркальнное");
+}
+else
+{
+    System.Console.WriteLine("Вы ввели не пятизначное чесло");
+}
---
+++ C#_DZ_03_02/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Напишите программу, которая принимает на вход координаты двух точек и находит расстояние
+ между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53*/
+
+System.Console.WriteLine("Ввдите координаты x точки A ");
+int[] coordsA = new int[3];
+coordsA[0] = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Ввдите координаты y точки A ");
+coordsA[1] = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Ввдите координаты z точки A ");
+coordsA[2] = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Ввдите координаты x точки B ");
+int[] coordsB = new int[3];
+coordsB[0] = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Ввдите координаты y точки B ");
+coordsB[1] = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Ввдите координаты z точки B ");
+coordsB[2] = Convert.ToInt32(Console.ReadLine());
+
+int distensX = (int)Math.Pow(coordsA[0] - coordsB[0], 2);
+int distensY = (int)Math.Pow(coordsA[1] - coordsB[1], 2);
+int distensZ = (int)Math.Pow(coordsA[2] - coordsB[2], 2);
+
+double Resalt = Math.Sqrt(distensX + distensY + distensZ);
+
+System.Console.WriteLine("Результат: " + Math.Round(Resalt, 2));
